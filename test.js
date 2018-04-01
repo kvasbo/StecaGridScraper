18be01cafb84e128931adea1befec6ca28eca45f
Audun Kvasbø <audun@kvasbo.no>
2018-04-01 14:23:05 +0200

    Clean up tests

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-const steca = require('./index');
+const Steca = require('./index');
 
-var mySteca = new steca("192.168.1.146");
+const mySteca = new Steca('192.168.1.146');
 
-mySteca.getEffect().then((data)=>console.log("now", data));
-mySteca.getProductionToday().then((data)=>console.log("today", data));
-mySteca.getProductionThisMonth().then((data)=>console.log("month", data));
-mySteca.getProductionThisYear().then((data)=>console.log("year", data));
-mySteca.getProductionTotal().then((data)=>console.log("total", data));
+mySteca.getEffect().then(data => console.log('now', data));
+mySteca.getProductionToday().then(data => console.log('today', data));
+mySteca.getProductionThisMonth().then(data => console.log('month', data));
+mySteca.getProductionThisYear().then(data => console.log('year', data));
+mySteca.getProductionTotal().then(data => console.log('total', data));
 
